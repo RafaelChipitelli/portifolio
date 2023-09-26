3ad4c230e00953919bb4cd14e1cf56b01e75de6b
Rafael Chipitelli <rafaelchipitelli.o.c27@gmail.com>
2023-09-26 20:03:08 -0300

    estilizando a sideBar

--- src/container/Sidebar/index.tsx
+++ src/container/Sidebar/index.tsx
@@ -1,12 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img
-      src="https://github.com/RafaelChipitelli.png"
-      alt="Foto Rafael Chipitelli"
-    />
-    <Titulo fontSize={20}>Rafael Chipitelli</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={16}>Rafael Chipitelli</Titulo>
+      <Paragrafo tipo="secundario" fontSize={14}>
+        RafaelChipitelli
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
